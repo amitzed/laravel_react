68ae8496e4662f4208ee9d7f34e08e1215fd06b6
Amit <amit@AZ-iMac.local>
2018-09-20 20:22:28 -0400

    Used laravel-included FAKER for fake-data

--- database/seeds/ItemsTableSeeder.php
+++ database/seeds/ItemsTableSeeder.php
@@ -11,6 +11,13 @@ class ItemsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 0; $i < 85; $i++) {
+          Item::create([
+            'name' => $faker->title,
+            'about' => $faker->paragraph,
+            'price' => $faker->randomNumber(4),
+            'stock' => $faker->boolean(85)
+          ]);
+        }
     }
 }
